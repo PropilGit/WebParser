26da5a9a170a6b31fbc7fbdf281bce73b22ccafa
Propil <vova.khlp@yandex.ru>
2021-08-17 15:50:08 +0700

    inventory parsing

--- Controllers/EGRPController.cs
+++ Controllers/EGRPController.cs
@@ -1,6 +1,4 @@
-﻿using AngleSharp.Dom;
-using AngleSharp.Html.Parser;
-using WebParser.Models.EGRP;
+﻿using WebParser.Models.EGRP;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -8,49 +6,83 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using WebParser.Services;
 
 namespace WebParser.Controllers
 {
     public class EGRPController : Controller
     {
-        
         public IActionResult Index()
         {
             return View();
         }
 
         [HttpPost]
-        public IActionResult Parse(IFormFile uploadedFile)
+        public IActionResult Common(IFormFile uploadedFile)
         {
-            //получение файла
-            string html = ReadHtmlString(uploadedFile);
-            if (html == null)
-            {
-                ViewBag.Message = "Ошибка загрузки файла";
-                return View("Error");
-            }
-
-            // считывание строк
-            var rows = GetElements(html, "table.t tbody tr");
-            if (rows == null)
-            {
-                ViewBag.Message = "Ошибка чтения файла";
-                return View("Error");
-            }
-
-            // парсинг списка имушества
-            List<Estate> estates = ParseAllEstates(rows);
-            if (estates == null)
-            {
-                ViewBag.Message = "Ошибка парсинга файла";
-                return View("Error");
-            }
+            //получение списка объектов недвижимости
+            List<Estate> estates = Parse(uploadedFile);
+            if (estates == null) return View("Error");
 
             // вывод данных
             if (estates.Contains(Estate.ErrorEstate)) ViewBag.Message = "В ходе парсинга возникли ошибки! Полученная таблица содержит некорректные значения!";
             else ViewBag.Message = "Парсинг завершился успешно";
+
             ViewBag.Estates = estates;
-            return View("Result");
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Inventory(IFormFile uploadedFile)
+        {
+            //получение списка объектов недвижимости
+            List<Estate> estates = Parse(uploadedFile);
+            if (estates == null) return View("Error");
+
+            // вывод данных
+            if (estates.Contains(Estate.ErrorEstate)) ViewBag.Message = "В ходе парсинга возникли ошибки! Полученная таблица содержит некорректные значения!";
+            else ViewBag.Message = "Парсинг завершился успешно";
+
+            ViewBag.Estates = estates;
+            return View();
+        }
+
+        [NonAction]
+        public List<Estate> Parse(IFormFile uploadedFile)
+        {
+            try
+            {
+                //получение файла
+                string html = ReadHtmlString(uploadedFile);
+                if (html == null)
+                {
+                    ViewBag.ErrMessage = "Ошибка загрузки файла";
+                    return null;
+                }
+
+                // считывание строк
+                var rows = EGRPParser.GetElements(html, "table.t tbody tr");
+                if (rows == null)
+                {
+                    ViewBag.ErrMessage = "Ошибка чтения файла";
+                    return null;
+                }
+
+                // парсинг списка имушества
+                List<Estate> estates = EGRPParser.ParseAllEstates(rows);
+                if (estates == null)
+                {
+                    ViewBag.ErrMessage = "Ошибка парсинга файла";
+                    return null;
+                }
+
+                return estates;
+            }
+            catch (Exception)
+            {
+                ViewBag.ErrMessage = "Непридвиденная ошибка";
+                return null;
+            }
         }
 
         [NonAction]
@@ -73,96 +105,5 @@ namespace WebParser.Controllers
             }
             
         }
-
-        [NonAction]
-        IHtmlCollection<IElement> GetElements(string html, string selector)
-        {
-            try
-            {
-                var parser = new HtmlParser();
-                var document = parser.ParseDocument(html);
-
-                return document.QuerySelectorAll(selector);
-            }
-            catch (Exception)
-            {
-                return null;
-            }
-        }
-
-        #region Parse
-
-        [NonAction]
-        List<Estate> ParseAllEstates(IHtmlCollection<IElement> rows)
-        {
-            try
-            {
-                int estateCounter = 1;
-                List<Estate> estates = new List<Estate>();
-                for (int r = 0; r < rows.Length;)
-                {
-                    int rowspan = Int32.Parse(rows[r].QuerySelector("td:nth-child(1)").GetAttribute("rowspan"));
-
-                    Estate estate = ParseSingleEstate(
-                        estateCounter,
-                        rows.Skip(r).Take(rowspan));
-
-                    if (estate == null)
-                    {
-                        estates.Add(Estate.ErrorEstate);
-                    }
-                    else
-                    {
-                        estates.Add(estate);
-                    }
-                    estateCounter++;
-
-                    r += rowspan;
-                }
-
-                return estates;
-            }
-            catch (Exception)
-            {
-                return null;
-            }
-        }
-
-        [NonAction]
-        Estate ParseSingleEstate(int estateIndex, IEnumerable<IElement> rows)
-        {
-            try
-            {
-                // элементы ДО ограничения права
-
-                string kadastrNum = rows.ElementAt(0).QuerySelector("td:nth-child(4)").Text();
-                string name = rows.ElementAt(1).QuerySelector("td:nth-child(2)").Text();
-                string purpose = rows.ElementAt(2).QuerySelector("td:nth-child(2)").Text();
-                string area = rows.ElementAt(3).QuerySelector("td:nth-child(2)").Text();
-                string address = rows.ElementAt(4).QuerySelector("td:nth-child(2)").Text();
-                string rightType = rows.ElementAt(5).QuerySelector("td:nth-child(3)").Text();
-                string gosRegDate = rows.ElementAt(6).QuerySelector("td:nth-child(2)").Text();
-                string gosRegNum = rows.ElementAt(7).QuerySelector("td:nth-child(2)").Text();
-                string gosRegBasis = rows.ElementAt(8).QuerySelector("td:nth-child(2)").Text();
-
-                //ограничение права
-                List<Estate.RightsRestriction> rR = new List<Estate.RightsRestriction>();
-                for (int e = 10; e < rows.Count(); e += 2)
-                {
-                    string rRType = rows.ElementAt(e).QuerySelector("td:nth-child(3)").Text();
-                    string rRGosRegNum = rows.ElementAt(e + 1).QuerySelector("td:nth-child(2)").Text();
-
-                    rR.Add(new Estate.RightsRestriction(rRType, rRGosRegNum));
-                }
-
-                return new Estate(estateIndex, kadastrNum, name, purpose, area, address, rightType, gosRegDate, gosRegNum, gosRegBasis, rR);
-            }
-            catch (Exception)
-            {
-                return null;
-            }
-        }
-
-        #endregion
     }
 }
---
+++ Services/EGRPParser.cs
@@ -0,0 +1,99 @@
+﻿using AngleSharp.Dom;
+using AngleSharp.Html.Parser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebParser.Models.EGRP;
+
+namespace WebParser.Services
+{
+    public class EGRPParser
+    {
+
+        public static IHtmlCollection<IElement> GetElements(string html, string selector)
+        {
+            try
+            {
+                var parser = new HtmlParser();
+                var document = parser.ParseDocument(html);
+
+                return document.QuerySelectorAll(selector);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public static List<Estate> ParseAllEstates(IHtmlCollection<IElement> rows)
+        {
+            try
+            {
+                int estateCounter = 1;
+                List<Estate> estates = new List<Estate>();
+                for (int r = 0; r < rows.Length;)
+                {
+                    int rowspan = Int32.Parse(rows[r].QuerySelector("td:nth-child(1)").GetAttribute("rowspan"));
+
+                    Estate estate = ParseSingleEstate(
+                        estateCounter,
+                        rows.Skip(r).Take(rowspan));
+
+                    if (estate == null)
+                    {
+                        estates.Add(Estate.ErrorEstate);
+                    }
+                    else
+                    {
+                        estates.Add(estate);
+                    }
+                    estateCounter++;
+
+                    r += rowspan;
+                }
+
+                return estates;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public static Estate ParseSingleEstate(int estateIndex, IEnumerable<IElement> rows)
+        {
+            try
+            {
+                // элементы ДО ограничения права
+
+                string kadastrNum = rows.ElementAt(0).QuerySelector("td:nth-child(4)").Text();
+                string name = rows.ElementAt(1).QuerySelector("td:nth-child(2)").Text();
+                string purpose = rows.ElementAt(2).QuerySelector("td:nth-child(2)").Text();
+                string area = rows.ElementAt(3).QuerySelector("td:nth-child(2)").Text();
+                string address = rows.ElementAt(4).QuerySelector("td:nth-child(2)").Text();
+                string rightType = rows.ElementAt(5).QuerySelector("td:nth-child(3)").Text();
+                string gosRegDate = rows.ElementAt(6).QuerySelector("td:nth-child(2)").Text();
+                string gosRegNum = rows.ElementAt(7).QuerySelector("td:nth-child(2)").Text();
+                string gosRegBasis = rows.ElementAt(8).QuerySelector("td:nth-child(2)").Text();
+
+                //ограничение права
+                List<Estate.RightsRestriction> rR = new List<Estate.RightsRestriction>();
+                for (int e = 10; e < rows.Count(); e += 2)
+                {
+                    string rRType = rows.ElementAt(e).QuerySelector("td:nth-child(3)").Text();
+                    string rRGosRegNum = rows.ElementAt(e + 1).QuerySelector("td:nth-child(2)").Text();
+
+                    rR.Add(new Estate.RightsRestriction(rRType, rRGosRegNum));
+                }
+
+                return new Estate(estateIndex, kadastrNum, name, purpose, area, address, rightType, gosRegDate, gosRegNum, gosRegBasis, rR);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+    }
+}
